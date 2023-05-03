cc2a0ce8825e99cb0aa4a45d9a42bd9146ab3c26
AntonZharkov <19antoni88@gmail.com>
2023-05-03 13:41:25 +0300

    Try to optimize query with select_related and prefetch_related

--- service/services/views.py
+++ service/services/views.py
@@ -8,11 +8,11 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    # queryset = Subscription.objects.all().select_related('client', 'client__user').only(
+    # queryset = Subscription.objects.all().select_related('client', 'plan', 'client__user').only(
     #     "id", "plan_id",
-    #     "client__company_name", "client__user__email"
-    # )
+    #     "client__company_name", "client__user__email", 'plan__plan_type')
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user') \
                                                     .only('company_name',
                                                           'user__email'))
