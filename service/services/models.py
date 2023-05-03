e55aeb52cb0ad77db66422447ea0e112daad1af1
AntonZharkov <19antoni88@gmail.com>
2023-05-03 18:39:21 +0300

    Add new celery task

--- service/services/models.py
+++ service/services/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
 from django.db.models.fields import related
+from .tasks import set_price
 
 from clients.models import Client
 
@@ -8,6 +9,18 @@ from clients.models import Client
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -26,6 +39,18 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return self.plan_type
 
@@ -34,6 +59,7 @@ class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f'{self.client} subscriptions is {self.service}, plan is {self.plan}'
